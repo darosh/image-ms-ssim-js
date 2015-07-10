c356c5efc5729d9c8111249908bd91e2a6413584
Jan Forst <ensonador@gmail.com>
2015-07-10 12:09:25 +0200

    gh-pages gulp task fix

--- gulp_tasks/gh-pages.js
+++ gulp_tasks/gh-pages.js
@@ -5,7 +5,8 @@ module.exports = function (gulp, plugins) {
 		return merge([
 			gulp.src('dist/*').pipe(gulp.dest('deploy/dist')),
 			gulp.src('test/images/*').pipe(gulp.dest('deploy/test/images')),
-			gulp.src('test/browser_test.html').pipe(gulp.dest('deploy/test'))
+			gulp.src('test/browser_test.html').pipe(gulp.dest('deploy/test')),
+			gulp.src('node_modules/image-mse/dist/image-mse.js').pipe(gulp.dest('deploy/node_modules/image-mse/dist'))
 		], gulp.src('deploy/**/*').pipe(plugins.ghPages({message: 'update', cacheDir: 'temp/deploy'})));
 	};
 };
